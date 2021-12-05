fed0bd5a5d8bc4743df699b7b70ea8fc8feff341
tannguyen <nguyenminhtan2340@gmail.com>
2021-12-05 15:05:59 +0700

    Add new project

---
+++ public/main.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu')
+const menuBtn = document.getElementById('menu-btn')
+
+menuBtn.addEventListener('click', () =>{
+    menu.classList.toggle('invisible')
+})
